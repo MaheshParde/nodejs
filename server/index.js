9cd2be1a004741c05388b6c544452d0eb12f5b3e
Rahul Gupta <93612063+rahulguptaft9@users.noreply.github.com>
2022-03-29 17:33:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb://js_mastery:123123123@192.168.122.143:27017/mydb';
+const CONNECTION_URL = 'mongodb://js_mastery:123123123@192.168.122.143:27017/admin';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
